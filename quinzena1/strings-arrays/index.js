c357e7c61cb4c1126c82b1344cc7009d1f99c52d
Let1801 <leticia18moreira@gmail.com>
2021-09-14 22:56:32 -0300

    quinzena1-aula05

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,65 @@
+//Exercicio 1
+// let nomeUsuario = prompt("Qual o seu nome?")
+// let emailUsuario = prompt("Qual o seu e-mail?")
+
+// console.log(`O e-mail ${emailUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeUsuario}!`)
+
+//Exercicio 2
+// let comidasFavoritas = [
+//     "Lasanha",
+//     "Pão",
+//     "Pastel",
+//     "Pizza",
+//     "Açai"
+// ]
+
+//a
+// console.log(comidasFavoritas)
+
+//b
+// console.log(`Essas são as minhas comidas preferidas:
+// ${comidasFavoritas[0]}
+// ${comidasFavoritas[1]}
+// ${comidasFavoritas[2]}
+// ${comidasFavoritas[3]}
+// ${comidasFavoritas[4]}
+// `)
+
+//c
+// let comidaPreferidaUsuario = prompt("Qual sua comida preferida?")
+// let novaComida = comidasFavoritas.splice(1,1,comidaPreferidaUsuario)
+
+// console.log(comidasFavoritas)
+
+//Exercicio 3
+
+//a
+// let listaDeTarefas = []
+
+//b
+// listaDeTarefas[0]= prompt ("Digite 3 tarefas que você precise realizar no dia. Digite a primeira:")
+// listaDeTarefas[1]= prompt ("Digite a segunda:")
+// listaDeTarefas[2]= prompt ("Digite a terceira:")
+
+//c
+//console.log(listaDeTarefas)
+
+//d
+//let tarefaRealizada = Number(prompt("Digite o índice 0, 1 ou 2 de uma tarefa que já foi realizada:"))
+
+//e
+// listaDeTarefas.splice(tarefaRealizada,1)
+
+//f
+// console.log(listaDeTarefas)
+
+//Desafio 1
+
+// let frase = prompt("Digite uma frase:")
+// console.log(frase.split(' '))
+
+//Desafio 2
+
+// let frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+// console.log(frutas.indexOf("Abacaxi"))
+// console.log(frutas.length)
