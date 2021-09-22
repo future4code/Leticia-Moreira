2e6ba05a86e3bc08b7c9270555a9e948fedda929
Leticia Oliveira <leticia18moreira@gmail.com>
2021-09-21 22:37:51 -0300

    quinzena2-aula07

---
+++ quinzena2/objetos/index.js
@@ -0,0 +1,75 @@
+//Exercicio 1
+///a
+// const pessoa = {
+//     nome : "Leticia",
+//     apelidos : ["Lele", "Let", "Lê"],
+    
+// }
+// function mensagem () {
+//     console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de: ${pessoa.apelidos}`)
+//    }
+
+// //b
+
+// const novosApelidos = {
+//     ...pessoa,
+//     apelidos2 : [ "Leleca", "Lets", "Letsgo"]
+// }
+
+// console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de: ${novosApelidos.apelidos2}`)
+
+// mensagem()
+
+//Exercicio 2
+//a
+
+// const informacao1 = {
+//     nome : "Leticia",
+//     idade : 26,
+//     profissao : "Estudante" ,
+// }
+
+// const informacao2 ={
+//     nome : "Julio",
+//     idade : 30,
+//     profissao : "Almoxarife",
+// }
+
+// //b
+// function informacoes(informacao1) {
+//     const todasInformacoes = [informacao1.nome, informacao1.nome.length, informacao1.idade, informacao1.profissao, informacao1.profissao.length]
+// return todasInformacoes
+// }
+
+// console.log(informacoes(informacao1))
+
+
+//Exercicio 3
+//a
+// const carrinho = []
+//b
+//  const fruta1 = {
+//     nome: "Mamao",
+//     disponibilidade: true,
+//  }
+//  const fruta2 = {
+//     nome: "Banana",
+//     disponibilidade: true,
+//  }
+//  const fruta3 = {
+//     nome: "Uva",
+//     disponibilidade: true,
+//  }
+
+//c
+// function sacolao(){
+//     carrinho.push(fruta1)
+//     carrinho.push(fruta2)
+//     carrinho.push(fruta3)
+//     return sacolao
+// }
+
+// sacolao()
+//d
+// console.log(carrinho)
+
