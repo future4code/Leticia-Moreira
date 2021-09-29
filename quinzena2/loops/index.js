2b6acd55d008498f83f7e2b4a00ed989c0053012
Leticia Oliveira <leticia18moreira@gmail.com>
2021-09-28 23:07:50 -0300

    quinzena2-aula09

---
+++ quinzena2/loops/index.js
@@ -0,0 +1,82 @@
+///Exercicio 1
+
+// let animalEstimacao = Number(prompt("Quantos animais de estimação você tem?"))
+// let listaNomesAnimais = []
+ //a
+// if (animalEstimacao === 0) {
+//     console.log("Que pena! Você pode adotar um pet!")
+// } 
+//b
+// else{
+
+// while (animalEstimacao > 0) {
+//    nomesAnimais = prompt("Digite os nomes de seus pets:");
+//    listaNomesAnimais.push(nomesAnimais)  
+//    animalEstimacao--   
+// }
+//c
+//  console.log(listaNomesAnimais)
+// }
+
+//Exercicio 2
+//let arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+//a
+// function imprimiArray () {
+//     console.log(arrayOriginal)
+// }
+// imprimiArray()
+
+//b
+// for(let item of arrayOriginal) { 
+// console.log(item / 10)
+// }
+
+//c
+// for(let pares of arrayOriginal) {
+//     if(pares % 2 === 0) {
+//     console.log(pares)
+// }
+// }
+
+//d
+// for(let i = 0; i < arrayOriginal.length; i++) {
+//     let textoArray = "O elemento do índex " +i+ " é: " +arrayOriginal[i]
+//     console.log(textoArray)
+// }
+
+//e
+
+
+// let valorMaximo = (arrayOriginal) => {
+//   let valorMaximo = -Infinity;
+
+//   for (let numeroMaior of arrayOriginal) {
+//     if (numeroMaior > valorMaximo) {
+//       valorMaximo = numeroMaior;
+//     }
+//   }
+
+//   return valorMaximo;
+// };
+
+
+// let valorMinimo = (arrayOriginal) => {
+//     let valorMinimo = +Infinity;
+  
+//     for (let numeroMenor of arrayOriginal) {
+//       if (numeroMenor < valorMinimo) {
+//         valorMinimo = numeroMenor;
+//       }
+//     }
+  
+//     return valorMinimo;
+//   };
+  
+//   console.log(`O maior número é ${valorMaximo(arrayOriginal)} e o menor é ${valorMinimo(arrayOriginal)}`);
+
+
+
+
+
+
