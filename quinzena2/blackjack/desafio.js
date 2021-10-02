93184ed9a9f9f4ceac21b7dbd8805aeb5f658366
Leticia Oliveira <leticia18moreira@gmail.com>
2021-10-01 21:28:37 -0300

    quinzena2-projeto-nosso-jogo

---
+++ quinzena2/blackjack/desafio.js
@@ -0,0 +1,51 @@
+
+ //* EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ 
+ 
+    //const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    //console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    //console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ 
+    //8 - Se as duas cartas iniciais do usuário ou do computador forem dois ases (A), as cartas devem ser sorteadas novamente.
+ 
+ 
+
+// const bemVindo = 
+//    console.log("Boas vindas ao jogo de Blackjack!")
+
+//  if(confirm("Quer iniciar uma nova rodada?")) {
+//     let cartaUsuario1 = comprarCarta()
+//     let cartaUsuario2 = comprarCarta()
+//     let pontuacaoUsuario = cartaUsuario1.valor + cartaUsuario2.valor
+   
+//     let cartaComputador1 = comprarCarta()
+//     let cartaComputador2 = comprarCarta()
+//     let pontuacaoComputador = cartaComputador1.valor + cartaComputador2.valor
+
+//     if (cartaUsuario1 & cartaUsuario2 === "A", cartaComputador1 & cartaComputador2 === "A"){
+//       confirm("Jogue novamente")}
+   
+//    else if(confirm("Suas cartas são", cartaUsuario1, cartaUsuario2, ". A carta revelada do computador é", cartaComputador1,
+//          "Deseja comprar mais uma carta?"
+//       ))
+//     console.log("Usuário - cartas:", cartaUsuario1.texto,"e", cartaUsuario2.texto, "- pontuação:", pontuacaoUsuario)
+//     console.log("Computador - cartas:", cartaComputador1.texto, "e", cartaComputador2.texto, "- pontuação:", pontuacaoComputador)
+   
+//     if(pontuacaoUsuario > pontuacaoComputador) {
+//      console.log("O usuário ganhou!")
+//     } else if (pontuacaoComputador > pontuacaoUsuario) {
+//     console.log("O computador ganhou!")
+//     } else {
+//     console.log("Empate")}
+//  }
+//  else {
+//        console.log("O jogo acabou!")
+ //}
+   
+     //console.log("Usuário - cartas:", cartaUsuario1.texto,"e", cartaUsuario2.texto, "- pontuação:", pontuacaoUsuario)
+       //console.log("Computador - cartas:", cartaComputador1.texto, "e", cartaComputador2.texto, "- pontuação:", pontuacaoComputador)
+
+
+      //  if(confirm(cartaUsuario1 & cartaUsuario2 === "A", cartaComputador1 & cartaComputador2 === "A")){
+      //    console.log("Jogue novamente")
