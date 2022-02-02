708593b613646152c3defd94d52018170e064733
Leticia Oliveira <leticia18moreira@gmail.com>
2022-02-01 22:43:30 -0300

    parte 1 setup

---
+++ site-sw/src/App.js
@@ -0,0 +1,24 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Detalhes dos personagens
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
