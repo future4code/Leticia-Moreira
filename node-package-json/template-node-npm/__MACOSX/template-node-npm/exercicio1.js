7f5f7e3418ffe4e6a2b8c14e580fe128dc69529b
Leticia Oliveira <leticia18moreira@gmail.com>
2022-03-17 23:09:02 -0300

    exercicios 1 e 2

---
+++ node-package-json/template-node-npm/__MACOSX/template-node-npm/exercicio1.js
@@ -0,0 +1,8 @@
+const nome = "Leticia"
+const idade = Number("27")
+const novaIdade = Number("27")+7
+
+const result = "Olá,"&nome&"! Você tem"&idade&"anos."
+console.log(result)
+const result2 = "Olá,"&nome&"! Você tem"&idade&"anos. Em sete anos você terá" &novaIdade& "."
+console.log(result2)
