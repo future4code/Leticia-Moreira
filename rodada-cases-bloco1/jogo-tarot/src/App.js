21a00f6398c78f35435cb675abf602b761ed4ffa
Leticia Oliveira <leticia18moreira@gmail.com>
2022-10-05 19:21:32 -0300

    mais

--- rodada-cases-bloco1/jogo-tarot/src/App.js
+++ rodada-cases-bloco1/jogo-tarot/src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <div>
+        
       <div class="inicio">
         <header>
             <h1>Personare</h1>
@@ -22,6 +23,7 @@ function App() {
             
             <section>
                 <div class="tarot">
+                    
                     <h4>O MAGO</h4>
                     <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano1.jpg" alt="O MAGO" />
                 </div>
@@ -37,6 +39,44 @@ function App() {
                     <h4>O IMPERADOR</h4>
                     <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano4.jpg" alt="O IMPERADOR" />
                 </div>
+                <div>
+                    <h4>O PAPA</h4>
+                    <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano5.jpg" alt="O PAPA" />
+                </div>
+                <div>
+                    <h4>OS ENAMORADOS</h4>
+                    <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano6.jpg" alt="OS ENAMORADOS" />
+                </div>
+                <div>
+                    <h4>O CARRO</h4>
+                    <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano7.jpg" alt="O CARRO" />
+                </div>
+                <div>
+                    <h4>A JUSTIÇA</h4>
+                    <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano8.jpg" alt="A JUSTIÇA" />
+                </div>
+                <div>
+                    <h4>O EREMITA</h4>
+                    <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano9.jpg" alt="O EREMITA" />
+                </div>
+                <div>
+                    <h4>A RODA DA FORTUNA</h4>
+                    <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano10.jpg" alt="A RODA DA FORTUNA" />
+                </div>
+                <div>
+                    <h4>A FORÇA</h4>
+                    <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano11.jpg" alt="A FORÇA" />
+                </div>
+                <div>
+                    <h4>O HOMEM PENDURADO</h4>
+                    <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano12.jpg" alt="O HOMEM PENDURADO" />
+                </div>
+                <div>
+                    <h4>O IMPERADOR</h4>
+                    <img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano4.jpg" alt="O IMPERADOR" />
+                </div>
+               
+
                              
             </section>
         </main>
---
+++ rodada-cases-bloco1/jogo-tarot/src/components/Card/index.tsx
@@ -0,0 +1,1 @@
+<img src= "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/arcano1.jpg" alt="O MAGO" />
